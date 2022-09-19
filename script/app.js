af85f5e77f3141d43bf9e73f653190d285882007
DavidCRW <vargadavid946@gmail.com>
2022-09-19 13:19:14 +0200

    update

--- script/app.js
+++ script/app.js
@@ -10,6 +10,9 @@ const colorPalette = ['black', 'yellow', 'green', 'purple', 'blue', 'orange'];
 
 sizeButtons.forEach(e => {
     e.addEventListener('click', () => {
+        sizeButtons.forEach(element => element.classList.remove('active'))
+        e.classList.add('active')
+        gameUI.innerHTML = '';
         squareSize = 256 / parseInt(e.getAttribute('data-id'));
         size = parseInt(e.getAttribute('data-id')) * parseInt(e.getAttribute('data-id'));
         for(let i = 0; i < size; i++){
@@ -18,30 +21,37 @@ sizeButtons.forEach(e => {
             square.setAttribute('style', `height: ${squareSize}px; width: ${squareSize}px`)
             gameUI.appendChild(square);
         }
+        game(option)
     })
 })
 optionButtons.forEach(e => {
     e.addEventListener('click', () => {
+        optionButtons.forEach(element => element.classList.remove('active'))
+        e.classList.add('active')
         option = e.getAttribute('data-id');
-        if(option == 'rainbow'){
-            document.querySelectorAll('.square').forEach(e => {
-                e.addEventListener('click', () => {
-                    let randomColor = colorPalette[Math.floor(Math.random() * colorPalette.length)];
-                    e.style.backgroundColor = randomColor;
-                })
-            })
-        }else if(option == 'ereaser'){
-            document.querySelectorAll('.square').forEach(e => {
-                e.addEventListener('click', () => {
-                    e.style.backgroundColor = 'white';
-                })
-            })
-        }else{
-            document.querySelectorAll('.square').forEach(e => {
-                for(let i = 0; i < size; i++){
-                    e.style.backgroundColor = 'white';
-                }
-            })
-        }
+        game(option)
     })
 })
+
+function game(option){
+        if(option == 'rainbow'){
+            document.querySelectorAll('.square').forEach(e => {
+            e.addEventListener('click', () => {
+                let randomColor = colorPalette[Math.floor(Math.random() * colorPalette.length)];
+                e.style.backgroundColor = randomColor;
+            })
+        })
+        }else if(option == 'ereaser'){
+            document.querySelectorAll('.square').forEach(e => {
+            e.addEventListener('click', () => {
+                e.style.backgroundColor = 'white';
+            })
+        })
+        }else{
+            document.querySelectorAll('.square').forEach(e => {
+            for(let i = 0; i < size; i++){
+                e.style.backgroundColor = 'white';
+            }
+        })
+    }
+}
